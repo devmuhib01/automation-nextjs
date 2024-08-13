d1f64ef9820da2d18a914bf0b0aadf595f6e8bea
Muhibur Rahman <124481890+devmuhib01@users.noreply.github.com>
2024-08-13 18:16:32 +0600

    all nodes added

---
+++ components/Nodes/AddToListNode.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NodeComponent from "@/components/Nodes/NodeComponent";
+import { Handle } from "@xyflow/react";
+
+const AddToListNode = ({ id, type, data }) => (
+  <>
+    <Handle type="target" position="top" />
+    <NodeComponent id={id} data={data} type={type} />
+
+    <Handle type="source" position="bottom" />
+  </>
+);
+
+export default AddToListNode;
--- constants/nodeTypes.js
+++ constants/nodeTypes.js
@@ -1,11 +1,31 @@
+import AddNote from "@/components/Nodes/AddNote";
+import AddPipelineNode from "@/components/Nodes/AddPipelineNode";
+import RemovePipelineNode from "@/components/Nodes/RemovePipelineNode";
+import AddTagNode from "@/components/Nodes/AddTagNode";
+import AddToListNode from "@/components/Nodes/AddToListNode";
+import AssignTaskNode from "@/components/Nodes/AssignTaskNode";
 import CreateContactNode from "@/components/Nodes/CreateContactNode";
 import EndNode from "@/components/Nodes/EndNode";
 import InputNode from "@/components/Nodes/InputNode";
+import MoveToListNode from "@/components/Nodes/MoveToListNode";
+import RemoveTagNode from "@/components/Nodes/RemoveTagNode";
 import UpdateContactNode from "@/components/Nodes/UpdateContactNode";
+import PipelineStageNode from "@/components/Nodes/PipelineStageNode";
+import EmailNode from "@/components/Nodes/EmailNode";
 
 export const nodeTypes = {
   input: InputNode,
   contact: CreateContactNode,
   updateContact: UpdateContactNode,
+  addToList: AddToListNode,
+  moveToList: MoveToListNode,
+  addTag: AddTagNode,
+  removeTag: RemoveTagNode,
+  addNote: AddNote,
+  assignTask: AssignTaskNode,
+  addPipeline: AddPipelineNode,
+  removePipeline: RemovePipelineNode,
+  pipelineStageChange: PipelineStageNode,
+  email: EmailNode,
   end: EndNode,
 };
